2ef779acf6ebd9612743c30e9acb7de4bfedd2d6
Anwesha ghosh <anwesha05cse@gmail.com>
2023-08-21 22:02:36 +0530

    my third commit reactwebsite

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import { Link } from 'react-router-dom';
-import banner from "../images/dish.avif";
+
 import '../style/Homestyle.css';
 const Home = () => {
   return (
     <>
       <Layout>
-      <div className="home" style={{backgroundImage:`url(${banner})`}}>
+      <div className="home" style={{backgroundImage:`url("./images/back.jpg")`}}>
         <div className="headercontainer">
           <h1>Food Delivery Website</h1>
           <p>Best Food in India, Try It Now </p>
